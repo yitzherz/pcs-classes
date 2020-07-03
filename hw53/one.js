cd0e6295a50e6e7ce5819794fb01ae476434ad75
yitzherz <yitzherz@gmail.com>
2020-07-02 21:46:18 -0400

    hw53js

---
+++ hw53/one.js
@@ -0,0 +1,5 @@
+'use strict'
+var name = 'yitzchok'
+var email = 'yitzherz@gmail.com'
+var result = Number(window.prompt('what is your age'))
+
---
+++ hw53/two.js
@@ -0,0 +1,4 @@
+'use strict'
+console.log(name)
+console.log(email)
+console.log(result)
