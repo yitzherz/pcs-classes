2cab8f342ac718e7077f736975f873e4943f577e
yitzherz <yitzherz@gmail.com>
2020-09-22 22:39:50 -0400

    hw68

---
+++ hw68/pictures.js
@@ -0,0 +1,28 @@
+/* global $ */
+(function () {
+    'use strict';
+
+    const search = $('#search');
+    const imagesElem = $('#pictures');
+    const SearchButton = $('#enterButton');
+
+    SearchButton.click(() => {
+        $.ajax({
+            url: `https://api.flickr.com/services/feeds/photos_public.gne?tags=${search.val()}&format=json&jsoncallback=?`,
+            dataType: 'jsonp'
+        })
+            .done(pictureData => {
+                console.log(pictureData);
+                $('ul').empty();
+
+                pictureData.items.forEach(picture => {
+                    console.log(picture.media.m);
+                    $(`<li><li/>`).appendTo(imagesElem)
+                        .text(picture.title)
+                        .append(`<img src="${picture.media.m}">`);
+
+                });
+            })
+            .catch(err => console.error(err));
+    });
+}());
