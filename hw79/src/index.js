4d6a26e483d2f951ff21d4d20f3a08e05beeaf59
yitzherz <yitzherz@gmail.com>
2020-11-19 22:29:01 -0500

    hw79

---
+++ hw79/src/index.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+(function () {
+    'use strict';
+
+    const printButton = $('#print');
+    const printElem = $('#output');
+    let times = 0;
+    printButton.click(() => {
+        printElem.text(`button was clicked ${++times} times`);
+    });
+}());
